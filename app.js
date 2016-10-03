f051f0fef8fe8597d393428d787106df2a9d7ffb
Rodrigo Perez <rodrigo.perez@avantrip.com>
2016-10-03 18:04:48 -0300

    adding pacientes and appointments routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var pacientes = require('./routes/pacientes');
+var turnos = require('./routes/turnos');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/pacientes', pacientes);
+app.use('/turnos', turnos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
