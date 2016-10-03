c68f9403fe3e5df38d20a5b424edc0e1942aa6d6
Rodrigo Perez <rodrigo.perez@avantrip.com>
2016-10-03 18:24:46 -0300

    fixing routes

--- routes/pacientes.js
+++ routes/pacientes.js
@@ -3,10 +3,29 @@
  */
 var express = require('express');
 var router = express.Router();
+var mocks = require('../models/mocksPacientes');
+var lista = mocks.listaPacientes;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    if (req.query.page) {
+        res.json(lista);
+    } else if (req.query.nro_documento) {
+        console.log(req.query.nro_documento);
+        var listPacients = {};
+        var index = 0;
+        console.log(lista.results);
+        for (index; index < lista.results; index++) {
+            if (lista.results[index].nro_documento === Int.parse(req.query.nro_documento)) {
+                listPacients  = lista.results[index];
+                break;
+            }
+        }
+        res.json(listPacients);
+    }
+    else {
+        res.json(lista);
+    }
 });
 
 module.exports = router;
